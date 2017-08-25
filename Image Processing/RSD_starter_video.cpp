471a0a6be7a45cdf88d3906a1cef6694df249cdd
mrpie95 <michael.pieniazek@gmail.com>
2017-08-25 12:59:32 +1000

    added my semi finished image processing

--- Image Processing/RSD_starter_video.cpp
+++ Image Processing/RSD_starter_video.cpp
@@ -1,24 +1,3 @@
-/*
-* RSD_starter_video.cpp
-*
-* This code was developed for students studying RME40003 Robot Systems Design at Swinburne
-* University of Technology in Melbourne, Australia
-* 
-* This code is largely based on the 'starter_video.cpp' code created by Ethan Rublee, Nov 23, 
-* 2010 for use with OpenCV software. Ethan Rublee's code comes packaged with the OpenCV files 
-* and can be found in ..\OpenCV\sources\samples\cpp\
-*
-* This is a simple starter sample  for using opencv, getting a video stream, displaying images,
-* reading and outputting pixel values, and modifying pixel values.
-* 
-*/
-
-/*
-* CODE MODIFICATIONS AND BUG FIXES
-* 24/07/2014 Code added by Michelle Dunn to read, output and modify pixel values
-* 21/08/2014 Comments added by Michelle Dunn for Swinburne students
-*/
-
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
@@ -32,6 +11,12 @@ using namespace std;
 
 //hide the local functions in an anon namespace
 namespace {
+	const SCREEN_WIDTH 640;
+	const SCREEN_HEIGHT 480;
+	const XTRANSLATION (SCREEN_WIDTH - SCREEN_HEIGHT);
+	const GRIDSIZE (SCREEN_WIDTH/3);
+
+
     void help(char** av) {
         cout << "\nThis program justs gets you started reading images from video\n"
             "Usage:\n./" << av[0] << " <video device number>\n"
@@ -44,37 +29,157 @@ namespace {
             << endl;
     }
 
-    int process(VideoCapture& capture) {
-        int n = 0;
-        char filename[200];
-        string window_name = "video | q or esc to quit";
-        cout << "press space to save a picture. q or esc to quit" << endl;
-        
+	class Block
+	{
+		int xPos, yPos, position;
+		std::string colour;
 
-        //image recognition variables
-        int blue_H_l = 0;
-        int blue_S_l = 0;
-        int blue_V_l = 0;
+	public:
+		void set_XandY(int, int);
+		void set_colour(std::string);
+		void set_position(int);
 
-        int blue_H_u = 180;
-        int blue_S_u = 255;
-        int blue_V_u = 255;
 
-		double area_size = 100;
+	void Block::set_XandY(int x, int y){
+		xPos = x;
+		yPos = y;
+	}
 
-        namedWindow(window_name, CV_WINDOW_KEEPRATIO); //resizable window;
-		namedWindow("original", CV_WINDOW_KEEPRATIO); //resizable window;
-        namedWindow("Controls", CV_WINDOW_KEEPRATIO); //resizable window;
+	void Block::set_colour(std::string col) {
+		colour = col;
+	}
 
+	void Block::set_position(int pos)
+	{
+		position = pos;
+	}
+
+	void drawHUD(Mat &image) {
+		//draw the vertical lines
+		for (int x = 0; x<2; y++ ){
+					line(image, Point(x*XTRANSLATION, 0) PointPoint(x*XTRANSLATION, GRIDSIZE), Scalar(20, 255, 255), 3, 8, 0)
+		}
+		//draw the horizontal lines
+		for (int y = 0; y<2; y++ ){
+					line(image, Point(y*XTRANSLATION, GRIDSIZE) PointPoint(y*XTRANSLATION=GRIDSIZE, GRIDSIZE), Scalar(20, 255, 255), 3, 8, 0)
+		}
+	}
+
+	//checks if the blocks are in a 3x3 grid and assings them according to 
+	//the relative frame and size of the camera image. - the camera must be aligned correctely
+	void AssignPositions(vector<Blocks> &blocks){
+		for (Block &blk : blocks){
+			for (int y = 0; y<2; y++ ){
+				for (int x = 0; x<2; x++ ){ 
+					if ((blk.xPos>=XTRANSLATION+(x*GRIDSIZE))&&(blk.xPos<XTRANSLATION+(x*GRIDSIZE))&&
+			    		(blk.y*yPos>=y)&&(blk.yPos<XTRANSLATION+(y*GRIDSIZE))){
+						blk.position = x+y;
+					}
+				}
+			}
+		}
+	}
+
+	vector<Block> getShapes(Mat image,int areaSize) {
+		
+		vector<Block> result;
+
+		//vectors used for stroing the contours (the shape binary images - white blobs)
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+          
+		//find all the shapes in the image
+		findContours(image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		int i, area = 0;
+
+		//goes through all the contours and draws all the ones larger then the threshold area
+		for( i= 0; i< contours.size(); i++ )
+		{
+				
+			Moments moment = moments((cv::Mat)contours[i]);
+			area = contourArea(contours[i]);
+
+			int xPos = moment.m10/area;
+			int yPos = moment.m01/area;
+
+			Block block;
+			block.set_values(xPos, yPos, "Yellow");
+			result.push_back(block);
+
+			//if (area > areaSize)
+				//circle(original, Point(xPos, yPos), 50, Scalar(255, 255, 255), CV_FILLED, 8, 0);					
+		}   
+		return result;
+	}
+
+	//get the binary image based on the asking colour
+	Mat getBinaryImage(Mat image, String colour) {
+
+		Mat result;
+
+		//default settings for each colour value - whole specturm
+		int H_lower = 0;
+		int S_lower = 0;
+		int V_lower = 0;
+
+		int H_higher = 179;
+		int S_higher = 255;
+		int V_higher = 255;
+
+		//define colour range based on the asking colour
+		if (colour == "Yellow") {
+			H_lower = 0;
+			S_lower = 50;
+			V_lower = 50;
+
+			H_higher = 30;
+			S_higher = 0;
+			V_higher = 0;
+		}
+		else if (colour == "Red") {
+			H_lower = 0;
+			S_lower = 50;
+			V_lower = 50;
+
+			H_higher = 30;
+			S_higher = 0;
+			V_higher = 0;
+		}
+		else if (colour == "Blue") {
+
+		}
+		else {
+
+		}
+
+		/*//sliders used for calibrating the colours
         createTrackbar("Blue H - Lower", "Controls", &blue_H_l, 179);
         createTrackbar("Blue S - Lower", "Controls", &blue_S_l, 255);
         createTrackbar("Blue V - Lowre", "Controls", &blue_V_l, 255);
 
         createTrackbar("Blue H - Upper ", "Controls", &blue_H_u, 179);
         createTrackbar("Blue S - Upper", "Controls", &blue_S_u, 255);
-        createTrackbar("Blue V - Upper", "Controls", &blue_V_u, 255);
+        createTrackbar("Blue V - Upper", "Controls", &blue_V_u, 255);*/
 
-		createTrackbar("Area", "Controls", &blue_V_u, 2000);
+		//Threshold the image based on the colour
+		inRange(image, Scalar(H_lower,S_lower,V_lower), Scalar(H_higher,S_higher,V_higher), result); 
+		return result;
+	}
+
+
+    int process(VideoCapture& capture) {
+        int n = 0;
+        char filename[200];
+        string window_name = "video | q or esc to quit";
+        cout << "press space to save a picture. q or esc to quit" << endl;
+
+		double area_size = 100;
+
+		//window definitions
+        namedWindow(window_name, CV_WINDOW_KEEPRATIO); //resizable window;
+		namedWindow("original", CV_WINDOW_KEEPRATIO); //resizable window;
+        namedWindow("Controls", CV_WINDOW_KEEPRATIO); //resizable window;
+		createTrackbar("Area", "Controls", &area_size, 2000);
         
         //Mat frame = imread("C:\MyPic.jpg",CV_LOAD_IMAGE_GRAYSCALE);
         Mat frame;
@@ -85,69 +190,47 @@ namespace {
 
 			if (frame.empty())
                 break;
-            
-            
+
+			//converts the from BGR colour space to the HSV colour space      
 			cvtColor(frame,frame, CV_BGR2HSV);
 
-			//morphological opening (removes small objects from the foreground)
+			//morphological opening (removes small contours from the image)
 			erode(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 			dilate(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 			erode(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 			dilate(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-           inRange(frame, Scalar(blue_H_l, blue_S_l, blue_V_l), Scalar(blue_H_u, blue_S_u, blue_V_u), frame); //Threshold the image
+			//list of images one for each colour being detected
+			vector<Mat> images;
+			images.push_back(getBinaryImage(frame, "yellow"));
+			images.push_back(getBinaryImage(frame, "blue"));
+			images.push_back(getBinaryImage(frame, "green"));
+			images.push_back(getBinaryImage(frame, "red"));
+			images.push_back(getBinaryImage(frame, "other"));
+			
+			//list of blocks
+			vector<Block> blocks;
 
-            vector<vector<Point> > contours;
-            vector<Vec4i> hierarchy;
-            //RNG rng(12345);
-            
-			findContours( frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-			int i, j = 0;
+			//find all blocks in all images
+			for (Mat image : images){
+				blocks.push_back(getShapes(image, area_size));
+			}
 
-			int number = 0;
+			//Assign Positions
+			AssignPositions(blocks);
 
-			cout << contours.size() << endl;
-
-            for( i= 0; i< contours.size(); i++ )
-            {
-				Moments moment = moments((cv::Mat)contours[i]);
-
-				area_size = contourArea(contours[i]);
-
-				if (area_size > 100)
-				{
-					number++;
-
-					cout << moment.m10 / area_size << endl;
-					cout << moment.m01 / area_size << endl;
-					//found.setYPos(moment.m01 / area);
+			/*for (vector<Mat>::const_iterator it = images.begin(); it != images.end(); ++it) {
 					
-					
-				}
+				blocks.push_back(getShapes());
+				i++;
+			}*/
+			//cout << *it << endl;
 
-				cout << number << endl;
-				
-                //double contourArea = (contours[i], false); 
-				
-
-                // Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-                // drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-            }   
-
-			//cout << "TEST" << endl;
-			//cout << j << endl;
-
-            //Calculate the moments of the thresholded image
-            //Moments oMoments = moments(frame); */
-
-
-
-
-           // frame = &processedFrame; 
-        
+			drawHUD(frame);
 
             imshow(window_name, frame);
 			imshow("original", original);
+
             char key = (char)waitKey(5); //delay N millis, usually long enough to display and capture input
             switch (key) {
         case 'q':
@@ -166,8 +249,9 @@ namespace {
         }
         return 0;
     }
-
 }
+
+
 
 int main(int ac, char** av) {
 
